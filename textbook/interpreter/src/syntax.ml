1152d4c4e8b121a42bbe87110a8a5af9d119b61e
diohabara <diohabara@gmail.com>
2021-12-06 10:19:58 +0900

    feat: follow chap03-4

--- textbook/interpreter/src/syntax.ml
+++ textbook/interpreter/src/syntax.ml
@@ -14,8 +14,12 @@ type exp =
   | BLit of bool
   | BinOp of binOp * exp * exp
   | IfExp of exp * exp * exp
+  | LetExp of id * exp * exp
 
-type program = Exp of exp
+type program =
+  | Exp of exp
+  | Decl of id * exp
+
 type tyvar = int
 
 type ty =
